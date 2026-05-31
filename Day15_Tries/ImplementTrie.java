dc892860ce5d29a38dd9b381fe80defc714169e9
kausikhussain <kausik1027@gmail.com>
2026-05-31 15:53:33 +0530

    Solved: Implement Trie (Prefix Tree)

---
+++ Day15_Tries/ImplementTrie.java
@@ -0,0 +1,50 @@
+package Day15_Tries;
+
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord = false;
+}
+
+public class ImplementTrie {
+    private TrieNode root;
+
+    public ImplementTrie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+            curr = curr.children[index];
+        }
+        curr.isEndOfWord = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                return false;
+            }
+            curr = curr.children[index];
+        }
+        return curr.isEndOfWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                return false;
+            }
+            curr = curr.children[index];
+        }
+        return true;
+    }
+}
